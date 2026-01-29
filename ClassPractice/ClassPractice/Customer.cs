ad31ca9bcf2b2a4d3147e641a52746bdb2f98906
SapereAudePr <capitolsmife@gmail.com>
2026-01-30 00:15:59 +0300

    dwqd

---
+++ ClassPractice/ClassPractice/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    class Customer
+    {
+        public string Name { get; set; }
+        public string[] KidNames { get; set; }
+        public string ContactNumber { get; set; }
+        public string Address { get; set; }
+        public int Password { get; set; }
+        public int Id { get; set; }
+
+        private static int IdNum = 0;
+
+        public Customer(string name, string[] kidNames, string contactNumber, string addess, int password)
+        {
+            Name = name;
+            KidNames = kidNames;
+            ContactNumber = contactNumber;
+            Address = addess;
+            Password = password;
+            Id = IdNum++;
+
+            //Console.WriteLine($"{Name}, {ContactNumber}, {Address}, {Password}, {Id}");
+        }
+    }
+}
---
+++ ClassPractice/ClassPractice/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ClassPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer[] customer = new Customer[]
+            {
+                new Customer("Raven",
+                new string[] {"Martha", "Ayse", "Eylul", "Canan"},
+                "58925812",
+                "Italy",
+                825892),
+                new Customer("John",
+                new string[] {"Kaan", "Rukiye"},
+                "21591258",
+                "UK",
+                52785),
+                new Customer("Alicia",
+                new string[] {"Hasan", "Ayfer", "Serkan", "Kadir"},
+                "568346",
+                "USA",
+                1258235),
+            };
+
+            StartProgram(customer);
+
+            Console.ReadKey();
+        }
+
+        private static void StartProgram(Customer[] customer)
+        {
+            Console.WriteLine("Welcome!");
+            Console.WriteLine($"Type \"displayDb\" to see the customer database");
+            string? userInput = Console.ReadLine();
+            if (userInput == "displayDb" && userInput != null)
+            {
+                DataBase db = new(customer);
+                db.DisplayDB();
+            }
+        }
+    }
+}
