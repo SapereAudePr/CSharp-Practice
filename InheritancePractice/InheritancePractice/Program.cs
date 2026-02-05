617a818e57eec8d844a6fd72cc07054b028eee54
SapereAudePr <capitolsmife@gmail.com>
2026-02-05 10:36:46 +0300

    Practice for inheritance continues.

---
+++ InheritancePractice/InheritancePractice/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace InheritancePractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new();
+            dog.MakeSound();
+
+            WolfDog wolfDog = new();
+            wolfDog.MakeSound();
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("A generic animal sound.");
+        }
+    }
+
+    // Has property of the Animal class
+    class Dog: Animal
+    {
+        public override void MakeSound()
+        {
+            base.MakeSound();
+            Eat();
+            Console.WriteLine("Dog Barking...");
+            //Eat();
+        }
+    }
+
+
+    // Has all the property of Animal and Dog classes
+    class WolfDog: Dog
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("WolfDog Barking...");
+            //Eat();
+            //Bark();
+        }
+    }
+
+    // Has property of the Animal class
+    class Cat : Animal
+    { 
+        public override void MakeSound()
+        {
+            Console.WriteLine("Cat Meow...");
+        }
+    }
+
+
+    class ParentClass
+    {
+        public int publicField;
+        protected int protectedField;
+        private int privateField;
+
+
+    }
+
+    class ChildClass: ParentClass
+    { 
+        public void Fnc()
+        {
+            publicField = 0;
+            protectedField = 0;
+            // private is only accessible in the class where it's declared.
+            // privateField = 0;
+        }
+    }
+
+}
---
+++ InheritancePractice2/InheritancePractice2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Threading.Tasks.Dataflow;
+using System.Xml.Linq;
+
+namespace InheritancePractice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person john = new Employee("John", "Doe", 30, 1485, "C# Developer", 100000);
+            john.DisplayInfo();
+
+            Employee raven = new Manager("Raven", "Smith", 43, 8537, "Lead Manager", 400000, "Art Manager", 12);
+            raven.DisplayInfo();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+
+            AgePlus(Age);
+        }
+
+        public int AgePlus(int age)
+        {
+            return age + 1;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name} | Surname: {Surname} | Age: {Age}");
+        }
+
+        
+    }
+
+    public class Employee: Person
+    {
+        public int Id { get; set; }
+        public string Job { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(string name, string surname, int age, int id, string job, int salary) : base(name, surname, age)
+        {
+            Id = id;
+            Job = job;
+            Salary = salary;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name} | Surname: {Surname} | Age: {Age} | ID: {Id} | Job: {Job} | Salary: {Salary}");
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public string Position { get; set; }
+        public int TeamSize { get; set; }
+
+        public Manager(string name, string surname, int age, int id, string job, int salary, string position, int teamSize) : base(name, surname, age, id, job, salary)
+        {
+            Position = position;
+            TeamSize = teamSize;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name} | Surname: {Surname} | Age: {Age} | ID: {Id} | Job: {Job} | Salary: {Salary} | Position: {Position} | Team Size: {TeamSize}");
+        }
+    }
+
+}
