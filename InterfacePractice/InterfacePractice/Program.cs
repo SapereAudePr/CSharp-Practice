40b7b26e6dc4ddb809aa4a3d2e654a769314046f
SapereAudePr <capitolsmife@gmail.com>
2026-02-06 10:05:04 +0300

    I'm trying to understand interfaces

--- InterfacePractice/InterfacePractice/Program.cs
+++ InterfacePractice/InterfacePractice/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace InterfacePractice
+﻿
+namespace InterfacePractice
 {
+    public interface IWriter
+    {
+        string CensorText(string text);
+    }
+
+    public class Censor : IWriter
+    {
+        string[] badWords =
+        [
+            "fuck",
+            "shit",
+        ];
+
+        public string CensorText(string text)
+        {
+            foreach (string word in badWords)
+            {
+                text = text.Replace(word, "[CENSORED]");
+            }
+            return text;
+        }
+    }
+
+    public class Chat
+    {
+        private readonly IWriter _innerWriter;
+
+        public Chat(IWriter innerWriter)
+        {
+            _innerWriter = innerWriter;
+        }
+        public void SendMessage(string message)
+        {
+            string censor = _innerWriter.CensorText(message);
+            Console.WriteLine(censor);
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IWriter writer = new Censor();
+            Chat chat = new Chat(writer);
+
+            chat.SendMessage("fuck you");
+
+            Console.ReadKey();
         }
     }
 }
