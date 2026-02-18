16158f1f6931b9fd7591b0fa3d29b04f035eed4a
SapereAudePr <capitolsmife@gmail.com>
2026-02-18 12:55:09 +0300

    //

---
+++ InterfaceDI-Practice/InterfaceDI-Practice/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace InterfaceDI_Practice
+{
+    internal class Program
+    {
+        public interface IToolInterface
+        {
+            public void SetHammer(Hammer hammer);
+            public void SetDrill(Drill drill);
+
+        }
+
+        public class Hammer
+        {
+            public void UseHammer()
+            {
+                Console.WriteLine("Using hammer");
+            }
+        }
+
+        public class Drill
+        {
+            public void UseDrill()
+            {
+                Console.WriteLine("Using drill");
+            }
+        }
+
+        public class Builder : IToolInterface
+        {
+            Hammer _hammer;
+            Drill _drill;
+
+            
+
+            public void SetHammer(Hammer hammer)
+            {
+                _hammer = hammer;
+            }
+
+            public void SetDrill(Drill drill)
+            {
+                _drill = drill;
+            }
+
+            public void Execute()
+            {
+                _hammer.UseHammer();
+                _drill.UseDrill();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Hammer hammer = new();
+            Drill drill = new();
+            Builder builder = new();
+
+            builder.SetDrill(drill);
+            builder.SetHammer(hammer);
+
+            builder.Execute();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StrucsPractice/StrucsPractice/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace StrucsPractice
+{
+    internal class Program
+    {
+        public struct Point
+        {
+            public int X;
+            public int Y;
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public void Display()
+            {
+                Console.WriteLine($"{X} | {Y}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Point p1 = new(10, 30);
+            p1.Display();
+
+            Point p2;
+            p2.X = 70;
+            p2.Y = 30;
+            p2.Display();
+
+            Point p3 = p2;
+            p3.Y = 80;
+            p3.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
