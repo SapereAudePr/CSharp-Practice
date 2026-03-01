5de4169e52d0baf689a8cdc32193c8c9c45b55fd
SapereAudePr <capitolsmife@gmail.com>
2026-03-01 05:43:38 +0300

    Backup..

--- RegexPracticeApp/RegexPractice/Program.cs
+++ RegexPracticeApp/RegexPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualBasic.FileIO;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 namespace RegexPractice
@@ -15,7 +16,7 @@ namespace RegexPractice
         static void CreateFile()
         {
             string desktop = Path.GetFullPath(SpecialDirectories.Desktop);
-            string fileName = "PhoneNums.txt";
+            string fileName = "PhoneNumbers.txt";
             string fileLocation = Path.Combine(desktop, fileName);
 
             string text = "" +
@@ -34,26 +35,44 @@ namespace RegexPractice
 
         static void MatchNumbers()
         {
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+
             string desktop = Path.GetFullPath(SpecialDirectories.Desktop);
-            string fileName = "PhoneNums.txt";
+            string fileName = "PhoneNumbers.txt";
             string fileLocation = Path.Combine(desktop, fileName);
 
             string trPhonePattern = @"\b?(?:\+|0)?\(?(?:\d{2,4})?\)?[ \.\-\#]?\d{3}[ \.\-\#]?\d{3}[ \.\-\#]?\d{2}[ \.\-\#]?\d{2}(?:\d{4})?\b";
             Regex regex = new(trPhonePattern);
 
-            List<string> validNums = [];
+            string[] toSearch = File.ReadAllLines(fileLocation);
 
-            foreach (var num in File.ReadAllLines(fileLocation))
+            List<string> validNumbers = [];
+
+            int invalidCount = 0;
+            foreach (var num in toSearch)
             {
-                if (regex.IsMatch(num)) validNums.Add(num);
+                if (regex.IsMatch(num.Trim()))
+                    validNumbers.Add(num);
+                else
+                    invalidCount++;
             }
 
-            Console.WriteLine($"{validNums.Count} Valid numbers found:\n");
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"{invalidCount} Invalid numbers found");
+            Console.ResetColor();
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"{validNumbers.Count} Valid numbers found:\n");
+            Console.ResetColor();
 
-            foreach (var num in validNums)
+            foreach (var num in validNumbers)
             {
                 Console.WriteLine(num);
             }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Milliseconds: {stopwatch.ElapsedMilliseconds}");
         }
     }
 }
