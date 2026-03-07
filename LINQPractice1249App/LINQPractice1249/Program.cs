f3632bdd6897cdbac0ffef2899c3dcc0f0640509
SapereAudePr <capitolsmife@gmail.com>
2026-03-07 11:22:43 +0300

    Before creating interface segregation principle practice

--- LINQPractice1249App/LINQPractice1249/Program.cs
+++ LINQPractice1249App/LINQPractice1249/Program.cs
@@ -75,6 +75,89 @@ namespace LINQPractice1249
                 student.Print();
             }
         }
+
+        public void SortByAge()
+        {
+            var sortByAge = students.OrderByDescending(x => x.Age);
+            //var sortByAge = from student in students
+            //                where student.Age > 15 
+            //                orderby student.Name 
+            //                select student;
+
+            foreach (Student s in sortByAge)
+            {
+                s.Print();
+            }
+        }
+
+        public void FromUniId1()
+        {
+            //var res = students.Where(x => x.UniId == 1);
+            //foreach (var s in res)
+            //{
+            //    s.Print();
+            //}
+
+            var x = from s in students
+                    join uni in universities on s.UniId equals uni.Id
+                    where uni.Name.Equals("Los Santos Uni")
+                    select s;
+
+            foreach (var y in x)
+            {
+                y.Print();
+            }
+        }
+
+        public void GetUsersFromUniId()
+        {
+            Console.WriteLine("Enter uni Id");
+            if (!int.TryParse(Console.ReadLine(), out int temp)) throw new Exception("Invalid input");
+            var x = from s in students
+                    where s.UniId.Equals(temp)
+                    select s;
+
+            foreach (var s in x)
+            {
+                s.Print();
+            }
+        }
+
+        //public void StAndUniCollection()
+        //{
+        //    var x = from s in students
+        //            join uni in universities
+        //            on s.UniId equals uni.Id
+        //            where uni.Id.Equals(1)
+        //            orderby s.Name descending
+        //            select new {x = s.Name, y = uni.Name};
+
+        //    var j = x.ToList();
+
+        //    foreach (var s in j)
+        //    {
+        //        var u = s.x.ToString();
+        //        var i = s.y.ToString();
+        //        Console.WriteLine($"{u} | {i}");
+        //    }
+        //}
+
+        public void StAndUniCollection()
+        {
+            var x = from s in students
+                    join uni in universities
+                    on s.UniId equals uni.Id
+                    where uni.Id.Equals(1)
+                    orderby s.Name descending
+                    select new { StudentName = s.Name, UniName = uni.Name };
+
+            foreach (var s in x)
+            {
+                var sName = s.StudentName;
+                var uniName = s.UniName;
+                Console.WriteLine($"{sName} | {uniName}");
+            }
+        }
     }
 
     internal class Program
@@ -84,6 +167,10 @@ namespace LINQPractice1249
             UniversityManager manager = new();
             //manager.MaleStudents();
             //manager.FemaleStudents();
+            //manager.SortByAge();
+            //manager.FromUniId1();
+            //manager.GetUsersFromUniId();
+            manager.StAndUniCollection();
 
             Console.ReadKey();
         }
