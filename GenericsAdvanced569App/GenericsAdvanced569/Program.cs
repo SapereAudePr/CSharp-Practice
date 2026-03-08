40307d887874f196b37f473a87187264b3119d66
SapereAudePr <capitolsmife@gmail.com>
2026-03-08 07:06:55 +0300

    Repo example

---
+++ GenericsAdvanced569App/GenericsAdvanced569/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace GenericsAdvanced569
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Repository<IEntity> repo = new();
+            Product product = new();
+            repo.Add(product);
+        }
+
+        class Product : IEntity
+        {
+            public int Id { get; }
+        }
+    }
+}
---
+++ GenericsAdvanced569App/GenericsAdvanced569/Reprository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAdvanced569
+{
+    internal interface IEntity
+    {
+        int Id { get; }
+    }
+
+    internal class Repository<T> where T : IEntity
+    {
+        private List<T> values = new();
+
+        public void Add(T entity)
+        {
+            values.Add(entity);
+        }
+    }
+}
