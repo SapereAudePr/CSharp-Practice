aaf96b1f04e0e35c2711e20d853d17cf0fe2a727
SapereAudePr <capitolsmife@gmail.com>
2026-08-28 18:47:51 +0300

    First example is done with 3 nested generic classes

---
+++ GenericsPractice5864/GenericsPractice5864/Box.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericsPractice5864;
+
+public class Box<T>(T contents)
+{
+    public T Contents { get; } = contents;
+}
+
+public class Delivery<T>(T package, bool arrived)
+{
+    public T Package { get; } = package;
+    public bool Arrived { get; } = arrived;
+
+    public static Delivery<T> Success(T package) => new(package, true);
+    public static Delivery<T> Failure() => new(default!, false);
+}
+
+public class Create<T>(List<T> items)
+{
+    public List<T> Items { get; } = items;
+    public int Counts => Items.Count;
+}
---
+++ GenericsPractice5864/GenericsPractice5864/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericsPractice5864
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var delivery = Delivery<Box<Create<string>>>.Success(
+                new Box<Create<string>>(
+                    new Create<string>(
+                        new List<string> {
+                            "test", "test2" })));
+
+
+            Console.WriteLine(delivery.Arrived);
+            Console.WriteLine(delivery.Package.Contents.Counts);
+
+            Console.WriteLine(string.Join(Environment.NewLine,
+                delivery.Package.Contents.Items));
+
+
+            Console.ReadLine();
+        }
+    }
+}
